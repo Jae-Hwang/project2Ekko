690503866267e37e1d26c662161c599dd34695fb
Airton Prado <airton.k.prado@gmail.com>
2020-01-20 13:26:58 -0500

    implemented findbyusernamepassword and save functions

--- ProjectEkkoAPI/src/main/java/com/revature/daos/IUserDao.java
+++ ProjectEkkoAPI/src/main/java/com/revature/daos/IUserDao.java
@@ -5,4 +5,6 @@ import com.revature.models.User;
 public interface IUserDao {
 	
 	public User findByUsernamePassword(String username, String password);
+	public void save(User u);
+	
 }
--- ProjectEkkoAPI/src/main/java/com/revature/daos/UserDao.java
+++ ProjectEkkoAPI/src/main/java/com/revature/daos/UserDao.java
@@ -1,7 +1,9 @@
 package com.revature.daos;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.daos.IUserDao;
 import com.revature.models.User;
@@ -12,9 +14,23 @@ public class UserDao implements IUserDao{
 	private SessionFactory sf;
 
 	@Override
+	@Transactional
 	public User findByUsernamePassword(String username, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		Session s = sf.getCurrentSession();
+		User user = (User) s.createNativeQuery("SELECT * " +
+					"FROM users " +
+					"WHERE username = :name AND password = :pass" )
+					.setParameter("name", username)
+					.setParameter("pass", password);
+		return user;
+	}
+
+	@Override
+	@Transactional
+	public void save(User u) {
+		Session s = sf.getCurrentSession();
+		s.save(u);
+		
 	}
 
 }
