5c132daedfd7d25d98b03fc9c4b426b192df479b
Jae-Hwang <jae.hwang@revature.com>
2020-01-20 21:21:20 -0500

    Implemented pagination

--- ProjectEkkoAPI/src/main/java/com/revature/drivers/TestDriver.java
+++ ProjectEkkoAPI/src/main/java/com/revature/drivers/TestDriver.java
@@ -14,27 +14,26 @@ import com.revature.models.Post;
 import com.revature.models.Reaction;
 
 public class TestDriver {
-	
+
 	public static Logger log = LogManager.getRootLogger();
 
 	public static void main(String[] args) {
 
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+
 		PostDao postDao = (PostDao) ac.getBean("postDaoImpl");
-		
-		postDao.save(new Post(0, "Test Post 1", null, new HashSet<Comment>(), new HashSet<Reaction>(), null));
-		
-		List<Post> posts = postDao.findAll(0);
-		
+
+//		for (int i = 0; i < 20; ++i) {
+//			postDao.save(new Post(0, "Test Post" + i, null, new HashSet<Comment>(), new HashSet<Reaction>(), null));
+//		}
+
+		List<Post> posts = postDao.findAll(2);
+
 		log.info(posts);
-		
+
 		for (Post post : posts) {
 			log.info(post.toString());
 		}
-		
-		
-		
-		
+
 	}
 }
