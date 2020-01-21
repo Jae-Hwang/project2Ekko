97314bdcb48be7e801072e3a98cf642f45212ba3
Airton Prado <airton.k.prado@gmail.com>
2020-01-20 20:02:39 -0500

    trying to pull

---
+++ ProjectEkkoAPI/src/main/java/com/revature/daos/FriendListDao.java
@@ -0,0 +1,23 @@
+package com.revature.daos;
+
+import java.util.List;
+
+import com.revature.models.FriendList;
+import com.revature.models.User;
+
+public class FriendListDao implements IFriendListDao {
+
+	@Override
+	public List<FriendList> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
--- ProjectEkkoAPI/src/main/java/com/revature/drivers/TestDriverUser.java
+++ ProjectEkkoAPI/src/main/java/com/revature/drivers/TestDriverUser.java
@@ -4,10 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.revature.daos.IUserDao;
+import com.revature.models.User;
 
 public class TestDriverUser {
 
 	ApplicationContext ac = new ClassPathXmlApplicationContext("../webapp/WEB-INF/applicationContext.xml");
 	
 	IUserDao dao = (IUserDao) ac.getBean("userDao");
+	
+	User a = new User(1,"airton","password");
+	dao.save(a); 
+	User u = dao.findByUsernamePassword("airton", "password");
 }
